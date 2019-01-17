d177057b32d1f49d31bb041c9174507c96a1f556
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2019-01-17 19:00:36 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,51 +1,35 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
-class SimpleLSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_dim, emb_dim, out_dim, do):
-        super().__init__()
-        self.embedding = nn.Embedding(vocab_size, emb_dim)
-        #self.embedding.weight.data.copy_(pretrained_vec) #load pretrained vec
-        #self.embedding.weight.requires_grad = False #make embedding non-trainable    
-        self.encoder = nn.LSTM(emb_dim, hidden_dim, num_layers=1, dropout=do, bidirectional=True)
-        self.linear = nn.Linear(hidden_dim*2, hidden_dim)
-        self.predictor = nn.Linear(hidden_dim, out_dim)
-    
-    def forward(self, seq):
-        emb = self.embedding(seq)
-        out, hidden  = self.encoder(emb) #initial h0, c0 is zero. _ : (hn,cn), hdn=output features for each timestep
-        out = (hidden[0, :, :] + hidden[1, :, :])
-        #print (out.shape)
-        feature = self.linear(out)
-        #print (feature.shape)
-        preds = self.predictor(feature)
-        #print (preds.shape)
-        return preds
-
 class BiLSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_dim, emb_dim, gpu=True, vec=None, dropout):
+    def __init__(self, vocab_size, hidden_dim, emb_dim, out_dim, batch_size, dropout, gpu=True, vec=None):
         super().__init__()
         self.gpu = gpu
+        self.batch_size = batch_size
         self.hidden_dim = hidden_dim
-        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.embed = nn.Embedding(vocab_size, emb_dim)
         if vec is not None:
             self.embed.weight.data.copy_(vec)
-        self.encoder = nn.LSTM(emb_dim, hidden_dim, dropout=dropout, bidirectional=True)
+        self.encoder = nn.LSTM(emb_dim, hidden_dim, bidirectional=True)
+        self.hidden2label = nn.Linear(hidden_dim*2, out_dim)
     
-    def init_hidden(self, bsize):
-        h0 = Variable(torch.zeros(1*2, bsize, self.hidden_dim))
-        c0 = Variable(torch.zeros(1*2, bsize, self.hidden_dim))
+    def init_hidden(self):
+        h0 = Variable(torch.zeros(1*2, self.batch_size, self.hidden_dim))
+        c0 = Variable(torch.zeros(1*2, self.batch_size, self.hidden_dim))
         if self.gpu:
             h0 = h0.cuda()
             c0 = c0.cuda()
         return (h0, c0)
     
     def forward(self, seq):
-        self.hidden = self.init_hidden(seq)
+        self.hidden = self.init_hidden()
         emb = self.embed(seq)
         out, hidden = self.encoder(emb, self.hidden)
-        last = (hidden[0, :, :] + hidden[1, :, :])
+        #last = (hidden[0, :, :] + hidden[1, :, :])
+        y = self.hidden2label(out[-1])
+        log_probs = F.log_softmax(y, dim=2)
         # last = out[:, :, :self.hidden_dim] + out[:, :, self.hidden_dim:]
-        return last
+        return log_probs
