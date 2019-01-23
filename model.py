77ad23e8ea99fa5fc5488b99341850a73faacad7
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2019-01-23 15:36:03 +0900

    add dropout, nlayers, etc

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 class BiLSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_dim, emb_dim, out_dim, batch_size, dropout, gpu=True, vec=None):
+    def __init__(self, vocab_size, hidden_dim, emb_dim, out_dim, batch_size, nlayers, bidir, dropout, gpu=True, vec=None):
         super().__init__()
         self.gpu = gpu
         self.batch_size = batch_size
@@ -13,9 +13,11 @@ class BiLSTM(nn.Module):
         self.embed = nn.Embedding(vocab_size, emb_dim)
         if vec is not None:
             self.embed.weight.data.copy_(vec)
-        self.encoder = nn.LSTM(emb_dim, hidden_dim, bidirectional=True)
+        self.encoder = nn.LSTM(emb_dim, hidden_dim, num_layers=nlayers, bidirectional=bidir, dropout=dropout)
         self.hidden2label = nn.Linear(hidden_dim*2, out_dim)
+        self.dropout = nn.Dropout(dropout)
     
+    #actually we probably dont need this
     def init_hidden(self):
         h0 = Variable(torch.zeros(1*2, self.batch_size, self.hidden_dim))
         c0 = Variable(torch.zeros(1*2, self.batch_size, self.hidden_dim))
@@ -26,10 +28,9 @@ class BiLSTM(nn.Module):
     
     def forward(self, seq):
         self.hidden = self.init_hidden()
-        emb = self.embed(seq)
-        out, hidden = self.encoder(emb, self.hidden)
-        #last = (hidden[0, :, :] + hidden[1, :, :])
-        y = self.hidden2label(out[-1])
-        log_probs = F.log_softmax(y, dim=1)
-        # last = out[:, :, :self.hidden_dim] + out[:, :, self.hidden_dim:]
-        return log_probs
+        emb = self.dropout(self.embed(seq))
+        out, (hidden, cell) = self.encoder(emb)
+        last = out[-1] #same
+        last = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)) #same
+        y = self.hidden2label(last.squeeze(0))
+        return y
